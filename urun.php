019e006f049bee4f5976aa083cfda83f649e346d
Samet Altudağ <112618782+sametaltundag@users.noreply.github.com>
2023-03-02 20:43:10 +0300

    urunler için php dosyası oluşturuldu

---
+++ urun.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patika |Odev 6</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css.map">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<?php echo "Naber" ?>
+
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/bootstrap.bundle.min.js.map"></script>
+</body>
+</html>
